766dde01d12b20de390e0887b61128efb10e327a
rwifeng <rw.ifeng@gmail.com>
2017-12-26 11:33:14 +0800

    add annotations

--- src/Pandora/Service/LogDbService.php
+++ src/Pandora/Service/LogDbService.php
@@ -15,6 +15,16 @@ final class LogDbService {
         $this->auth = $auth;
     }
 
+    /**
+     * @param string $queryString   查询表达式  eg: field1:field_val
+     * @param string $sortByField   指定排序的字段   eg: field1:asc,field2:desc
+     * @param int $offsetFrom    返回的日志开始的位置
+     * @param int $limitSize     返回的数据数量
+     * @param array $fields  指定只返回哪些字段 eg: array("field1", "field2", ...)
+     * @return \Pandora\Http\Response
+     *
+     * 参考 https://qiniu.github.io/pandora-docs/#/api/logdb?id=%e6%9f%a5%e8%af%a2%e6%97%a5%e5%bf%97
+     */
     public function searchLogs($queryString, $sortByField, $offsetFrom, $limitSize, array $fields = array()) {
         $path = "/v5/repos/$this->repoName/search";
 
